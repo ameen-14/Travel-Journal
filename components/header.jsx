167f4319ace5893d3326343cbf3c63f5ac542924
Ameen <Ameen@NRH2F0690D.NRRSPL.NET>
2025-12-26 17:30:37 +0530

    First Commit

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+function Header (){
+    return (
+       <>
+       <nav className='nav-items'>
+        <header className="sym-header">
+            <img src='/images/globe.png' alt='image-logo'/>
+            <h1 className="header" > 
+                 My Travel Journal.
+            </h1>
+            </header>
+       </nav> 
+         
+       </>  
+    )
+}
+export default Header
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from '/components/Header.jsx'
+import Entry from '/components/Entry.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Entry />
+    </>
+  )
+}
+
+export default App
